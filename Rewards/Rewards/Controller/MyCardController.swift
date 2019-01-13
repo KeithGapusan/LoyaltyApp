573507da51e44b22aa464fe4f3b68f53e0640ccc
keith31 <ksgapusan@straightarrow.com.ph>
2019-01-13 21:48:14 +0800

    Update: Implementation of My card collectionview section.

--- Rewards/Rewards/Controller/MyCardController.swift
+++ Rewards/Rewards/Controller/MyCardController.swift
@@ -9,22 +9,55 @@
 import UIKit
 
 class MyCardController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    let cell_id = "robinsons_card_id"
+    lazy var collectionView :  UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 0
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = 0
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+     
+        cv.backgroundColor = .white
+        return cv
+    }()
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    fileprivate func setupCollectionView() {
+        view.addSubview(collectionView)
+        collectionView.register(RobinsonsCardCollectionViewCell.self, forCellWithReuseIdentifier: cell_id)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        view.addConstraintsWithFormat(format: "V:|[v0]|", views: collectionView)
+        view.addConstraintsWithFormat(format: "H:|[v0]|", views: collectionView)
+        
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        setupCollectionView()
 
+    }
+}
+
+extension MyCardController: UICollectionViewDelegate , UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cell_id, for: indexPath) as? RobinsonsCardCollectionViewCell{
+            
+            return cell
+        }
+        
+        return UICollectionViewCell()
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.size.width, height: collectionView.frame.size.height / 3)
+    }
+    
 }
---
+++ Rewards/Rewards/Model/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Rewards
+//
+//  Created by Keith Randell Gapusan on 13/01/2019.
+//  Copyright © 2019 Keith Randell Gapusan. All rights reserved.
+//
+
+import Foundation
+
+class Card: NSObject {
+    var id : Int?
+    var points : Int
+    var qr_url : String
+    var user : User?
+}
