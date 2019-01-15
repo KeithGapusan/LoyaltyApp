2ba2ae83cff6b1386b07c1e835925d725671e607
Keith Randell Gapusan <keithrandellgapusan@keiths-macbook.local>
2019-01-15 18:56:47 +0800

    Update: Profile controller- implementation of stretchy header  and Generic cells has been removed to the favorites

--- Rewards/Rewards/Controller/ProfileController.swift
+++ Rewards/Rewards/Controller/ProfileController.swift
@@ -10,21 +10,97 @@ import UIKit
 
 class ProfileController: UIViewController {
 
+    private let sectionInsets = UIEdgeInsets(top: 200.0,
+                                             left: 0.0,
+                                             bottom: 0.0,
+                                             right: 0.0)
+    
+    let itemsPerRow:  CGFloat = 1.0
+    lazy var collectionView: UICollectionView = {
+        var layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        cv.backgroundColor = .white
+        return cv
+    }()
+    
+    let imageView =  UIImageView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.addSubview(collectionView)
+        view.addConstraintsWithFormat(format: "V:|[v0]|", views: collectionView)
+        view.addConstraintsWithFormat(format: "H:|[v0]|", views: collectionView)
+        
+        //imageView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 300)
+        imageView.image = UIImage.init(named: "coffee")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        view.addSubview(imageView)
+        view.addConstraintsWithFormat(format: "V:|[v0(200)]", views: imageView)
+        view.addConstraintsWithFormat(format: "H:|[v0]|", views: imageView)
+        
+        collectionView.register(GenericCell.self, forCellWithReuseIdentifier: "id")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.contentInset = sectionInsets
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+extension ProfileController: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "id", for: indexPath) as! GenericCell
+        
+        
+        return cell
+        
+    }
+    
+    
+}
+
+extension ProfileController: UICollectionViewDelegateFlowLayout{
+    func sizeEqualSpacing(_ collectionView: UICollectionView, itemsPerRow: CGFloat, sectionInsets: UIEdgeInsets, view: UIView) -> CGSize{
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        let lower : UInt32 =  UInt32(collectionView.frame.size.height / 3)
+        let upper : UInt32 =  UInt32(collectionView.frame.size.height)
+        //  let randomNumber = arc4random_uniform(upper - lower) + lower
+        return CGSize(width: widthPerItem, height: CGFloat(lower))
+        
+    }
+    
+    fileprivate func cellSizeEqualSpaces(_ collectionView: UICollectionView) -> CGSize {
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        let lower : UInt32 =  UInt32(collectionView.frame.size.height / 3)
+        let upper : UInt32 =  UInt32(collectionView.frame.size.height)
+        //  let randomNumber = arc4random_uniform(upper - lower) + lower
+        return CGSize(width: widthPerItem, height: CGFloat(lower))
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //  return sizeEqualSpacing(collectionView, itemsPerRow: 2, sectionInsets: sectionInsets, view: self.view)
+        return cellSizeEqualSpaces(collectionView)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let y = 200 - (scrollView.contentOffset.y + 50)
+        let height = min(max(y, 60), 300)
+        imageView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: height )
+        
+        
+    }
 
 }
--- Rewards/Rewards/Model/User.swift
+++ Rewards/Rewards/Model/User.swift
@@ -10,19 +10,21 @@ import Foundation
 
 
 class User:NSObject{
-    var id : Int
-    var first_name:  String
-    var last_name: String
-    var middle_name: String
-    var birth_date: Date
-    var profile_url : String
+    var id : Int?
+    var first_name:  String?
+    var last_name: String?
+    var middle_name: String?
+    var birth_date: Date?
+    var profile_url : String?
     
     init(json: [String:Any]){
-        self.id = json["id"] as? String
+        self.id = json["id"] as? Int
         self.first_name = json["first_name"] as? String
         self.last_name = json["last_name"] as? String
         self.middle_name = json["middle_name"] as? String
-        self.birth_date = json["birtdate"] as? Double
+        if let date =  json["birtdate"] as? Double{
+           self.birth_date = Date(timeIntervalSinceReferenceDate: date)
+        }
         self.profile_url = json["profile_photo"] as? String
     }
     
